cfc506d580c6b25e8e2d0c1c30eab65078f8b2bd
Dezeabasili <ezeabasili@yahoo.co.uk>
2023-12-30 12:25:01 -0600

    edited picture.js in routes directory second time

--- controllers/picturesController.js
+++ controllers/picturesController.js
@@ -1,23 +1,23 @@
 const path = require("path");
 
 const getCities = async (req, res, next) => {
-    try {
-        const cityName = req.params.cityname
-        let filePath;
+  try {
+    const cityName = req.params.cityname;
+    let filePath;
 
-        filePath = path.join(
-            __dirname,
-            "..",
-            "public",
-            "hotel-cities",
-            cityName,
-            ".jpg"
-          );
-          res.status(200).sendFile(filePath);
+    filePath = path.join(
+      __dirname,
+      "..",
+      "pictures",
+      "hotel-cities",
+      cityName,
+      ".jpg"
+    );
 
-    } catch (err) {
-        next(err)
-    }
-}
+    res.status(200).sendFile(filePath);
+  } catch (err) {
+    next(err);
+  }
+};
 
-module.exports = {getCities}
+module.exports = { getCities };
