1314b27124ee8a19a48c9e2583d3d7cff04dad03
Dezeabasili <ezeabasili@yahoo.co.uk>
2023-12-30 18:52:39 -0600

    Updated Cities.js img tag the sixth time

--- server.js
+++ server.js
@@ -83,6 +83,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use('/hotel-cities/', express.static(path.join(__dirname, "hotel-cities")));
 app.use('/hotel-types/', express.static(path.join(__dirname, "hotel-types")));
 app.use('/hotelsPictures/', express.static(path.join(__dirname, "hotelsPictures")));
+app.use('/roomsPictures/', express.static(path.join(__dirname, "roomsPictures")));
 // app.use(express.static("public"));
 // app.use('/api/v1/pictures', express.static("public"));
 
